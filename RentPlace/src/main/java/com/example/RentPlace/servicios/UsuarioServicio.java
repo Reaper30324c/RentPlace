fef89d1fb756657c0b6483e248a91ebee4db4629
Daniel <reaper30324c@gmail.com>
2025-04-23 15:19:49 -0500

    Creacion de el proyecto integrador
    se crea el proyecto integrador en inelij y repositorio

---
+++ RentPlace/src/main/java/com/example/RentPlace/servicios/UsuarioServicio.java
@@ -0,0 +1,10 @@
+package com.example.RentPlace.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioServicio {
+    @Autowired
+
+}
