b236a7114beea97de944a36bcc57c6783194b6a0
Daniel <111089226+Reaper30324c@users.noreply.github.com>
2025-04-23 14:27:31 -0500

    Add files via upload

---
+++ RentPlace/src/main/java/com/example/RentPlace/RentPlaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.RentPlace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentPlaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentPlaceApplication.class, args);
+	}
+
+}
---
+++ RentPlace/src/test/java/com/example/RentPlace/RentPlaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RentPlace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentPlaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
